1a5f4e08379ded2d74824887f9b5f7a6e1404fe8
sprockmonty <sprockmonty@gmail.com>
2020-04-07 16:03:31 +0100

    added vandermonde poly method

--- juliaFiles/ijocs/src/polynomials.jl
+++ juliaFiles/ijocs/src/polynomials.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using Polynomials
+using Reduce # for genvandpoly macro 
 import Base.push!
 import Base.in
 import Base.intersect
@@ -178,6 +179,69 @@ function lgr_points(order) # does not include endpoints
         return pushfirst!(eigvals(J),-1)
     end
 end
+
+# The Vandermonde matrix method
+
+macro genvandpoly(index,leny,x,y) # generate poly functions using the Vandermonde matrix method
+    leny = :($leny)
+    symX = []
+    symY = []
+    A = Array{Expr}(undef, leny, leny)
+    for i = 1:leny
+        push!(symX, Meta.parse("x$i"))
+        push!(symY, Meta.parse("y$i"))
+    end
+    for i = 1:leny
+        for j = 1:leny
+            pwr = leny-j
+            var = symX[i]
+            A[i,j] = :($var^$pwr)
+        end
+    end
+    coefs = Algebra.:*(Algebra.inv(A),symY) # invert to find coefs of polynomial
+    for i = 1:leny
+        str = repr(coefs[i])
+        for j = 1:leny
+            str = replace(str,"y"*string(j)=>"$y"*"["*string(j)*"]")
+            str = replace(str,"x"*string(j)=>"$x"*"["*string(j)*"]")
+        end
+        coefs[i] = Meta.parse(chop(str, head=2, tail=1))
+    end
+
+    return coefs[:($index)]
+end
+
+function create_vand_poly(x,y, b::Bound)
+    xLength = length(x)
+    if xLength == 2
+        c1 = @genvandpoly(1, 2,:($x),:($y))
+        c2 = @genvandpoly(2, 2,:($x),:($y))
+        return create_coef_poly([c2,c1], b)
+    elseif xLength == 3
+        @bp
+        c1 = @genvandpoly(1, 3,:($x),:($y))
+        c2 = @genvandpoly(2, 3,:($x),:($y))
+        c3 = @genvandpoly(3, 3,:($x),:($y))
+        return create_coef_poly([c3,c2,c1], b)
+    elseif xLength == 4
+        c1 = @genvandpoly(1, 4,:($x),:($y))
+        c2 = @genvandpoly(2, 4,:($x),:($y))
+        c3 = @genvandpoly(3, 4,:($x),:($y))
+        c4 = @genvandpoly(4, 4,:($x),:($y))
+        return create_coef_poly([c4,c3,c2,c1], b)
+    elseif xLength == 5
+        c1 = @genvandpoly(1, 5,:($x),:($y))
+        c2 = @genvandpoly(2, 5,:($x),:($y))
+        c3 = @genvandpoly(3, 5,:($x),:($y))
+        c4 = @genvandpoly(4, 5,:($x),:($y))
+        c5 = @genvandpoly(5, 5,:($x),:($y))
+        return create_coef_poly([c5,c4,c3,c2,c1], b)
+    else 
+        println("poly order outside create_exactfit_poly range")
+        return 0 # find a better return value
+    end
+end
+create_vand_poly(x,y) = create_vand_poly(x,y,Bound(-Inf, Inf))
 
 
 ## temp code
--- juliaFiles/ijocs/src/polynomials2.jl
+++ juliaFiles/ijocs/src/polynomials2.jl
@@ -1,81 +1,35 @@
-using ForceImport
-using Reduce
-
-macro genPolyFit(index,leny,x,y)
-    leny = :($leny)
-    symX = []
-    symY = []
-    A = Array{Expr}(undef, leny, leny)
-    for i = 1:leny
-        push!(symX, Meta.parse("x$i"))
-        push!(symY, Meta.parse("y$i"))
-    end
-    for i = 1:leny
-        for j = 1:leny
-            pwr = leny-j
-            var = symX[i]
-            A[i,j] = :($var^$pwr)
-        end
-    end
-    coefs = Algebra.:*(Algebra.inv(A),symY)
-    for i = 1:leny
-        str = repr(coefs[i])
-        for j = 1:leny
-            str = replace(str,"y"*string(j)=>"y"*"["*string(j)*"]")
-            str = replace(str,"x"*string(j)=>"x"*"["*string(j)*"]")
-        end
-        coefs[i] = Meta.parse(chop(str, head=2, tail=1))
-    end
-
-    return coefs[:($index)]
-end
-### macro test code
-f = [1,2]
-t = [0,4]
-@macroexpand @genPolyFit(1, 2,x,y)
-
-
-function createpoly2(x,y, b::Bound)
-    xLength = length(x)
-    if xLength == 2
-        c1 = @genPolyFit(1, 2,:x,:y)
-        c2 = @genPolyFit(2, 2,:x,:y)
-        return create_coef_poly([c2,c1], b)
-    end
-end
-@benchmark createpoly(f,t,Bound(-100,100))
-###
 
 
 
-function createpoly(x,y, b::Bound)
-    xLength = length(x)
-    if xLength == 2
-        pa = (-y[1]x[2] + y[2]*x[1]) / (x[1] - x[2])
-        pb = (y[1] - y[2]) / (x[1] - x[2])
-        return create_coef_poly([pa,pb], b)
-    elseif xLength == 3
-        pa = -y[1]*(x[2]*x[3])/(x[1]*x[2] + x[1]*x[3] - x[2]*x[3] - x[1]^2) -y[2]*(x[1]*x[3])/(x[1]*x[2] - x[1]*x[3] + x[2]*x[3] - x[2]^2) + y[3]*(x[1]*x[2])/(x[1]*x[2] - x[1]*x[3] - x[2]*x[3] + x[3]^2)
-        pb = y[1]*(x[2] + x[3])/(x[1]*x[2] + x[1]*x[3] - x[2]*x[3] - x[1]^2) + y[2]*(x[1] + x[3])/(x[1]*x[2] - x[1]*x[3] + x[2]*x[3] - x[2]^2) -y[3]*(x[1] + x[2])/(x[1]*x[2] - x[1]*x[3] - x[2]*x[3] + x[3]^2)
-        pc =  -y[1]/(x[1]*x[2] + x[1]*x[3] - x[2]*x[3] - x[1]^2) -y[2]/(x[1]*x[2] - x[1]*x[3] + x[2]*x[3] - x[2]^2) + y[3]/(x[1]*x[2] - x[1]*x[3] - x[2]*x[3] + x[3]^2)
-        return create_coef_poly([pa,pb,pc], b)
-    elseif xLength == 4
-        pa = y[1]*(x[2]*x[3]*x[4])/(x[1]^2*x[2] + x[1]^2*x[3] + x[1]^2*x[4] - x[1]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] + x[2]*x[3]*x[4])+ y[2]*(x[1]*x[3]*x[4])/(x[1]*x[2]^2 + x[2]^2*x[3] + x[2]^2*x[4] - x[2]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] + x[1]*x[3]*x[4] - x[2]*x[3]*x[4])+ y[3]*(x[1]*x[2]*x[4])/(x[1]*x[3]^2 + x[2]*x[3]^2 + x[3]^2*x[4] - x[3]^3 - x[1]*x[2]*x[3] + x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4])+ y[4]*(x[1]*x[2]*x[3])/(x[1]*x[4]^2 + x[2]*x[4]^2 + x[3]*x[4]^2 - x[4]^3 + x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4])
-        pb = -y[1]*(x[2]*x[3] + x[2]*x[4] + x[3]*x[4])/(x[1]^2*x[2] + x[1]^2*x[3] + x[1]^2*x[4] - x[1]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] + x[2]*x[3]*x[4]) -y[2]*(x[1]*x[3] + x[1]*x[4] + x[3]*x[4])/(x[1]*x[2]^2 + x[2]^2*x[3] + x[2]^2*x[4] - x[2]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] + x[1]*x[3]*x[4] - x[2]*x[3]*x[4]) -y[3]*(x[1]*x[2] + x[1]*x[4] + x[2]*x[4])/(x[1]*x[3]^2 + x[2]*x[3]^2 + x[3]^2*x[4] - x[3]^3 - x[1]*x[2]*x[3] + x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4]) -y[4]*(x[1]*x[2] + x[1]*x[3] + x[2]*x[3])/(x[1]*x[4]^2 + x[2]*x[4]^2 + x[3]*x[4]^2 - x[4]^3 + x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4])
-        pc = y[1]*(x[2] + x[3] + x[4])/(x[1]^2*x[2] + x[1]^2*x[3] + x[1]^2*x[4] - x[1]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] + x[2]*x[3]*x[4])+ y[2]*(x[1] + x[3] + x[4])/(x[1]*x[2]^2 + x[2]^2*x[3] + x[2]^2*x[4] - x[2]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] + x[1]*x[3]*x[4] - x[2]*x[3]*x[4])+ y[3]*(x[1] + x[2] + x[4])/(x[1]*x[3]^2 + x[2]*x[3]^2 + x[3]^2*x[4] - x[3]^3 - x[1]*x[2]*x[3] + x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4])+y[4]*(x[1] + x[2] + x[3])/(x[1]*x[4]^2 + x[2]*x[4]^2 + x[3]*x[4]^2 - x[4]^3 + x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4])
-        pd = -y[1]/(x[1]^2*x[2] + x[1]^2*x[3] + x[1]^2*x[4] - x[1]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] + x[2]*x[3]*x[4]) -y[2]/(x[1]*x[2]^2 + x[2]^2*x[3] + x[2]^2*x[4] - x[2]^3 - x[1]*x[2]*x[3] - x[1]*x[2]*x[4] + x[1]*x[3]*x[4] - x[2]*x[3]*x[4]) -y[3]/(x[1]*x[3]^2 + x[2]*x[3]^2 + x[3]^2*x[4] - x[3]^3 - x[1]*x[2]*x[3] + x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4]) -y[4]/(x[1]*x[4]^2 + x[2]*x[4]^2 + x[3]*x[4]^2 - x[4]^3 + x[1]*x[2]*x[3] - x[1]*x[2]*x[4] - x[1]*x[3]*x[4] - x[2]*x[3]*x[4])
-        return create_coef_poly([pa,pb,pc,pd],b)
 
-    end
-end
-createpoly(x,y) = createpoly(x,y,Bound(-Inf, Inf))
+
+
 
 
 ### test code
 
-@benchmark polyfit([-1,0,1,2],[1,2,4,20])
-@benchmark createpoly([-1,0,1,2],[1,2,4,20])
-@benchmark create_lagrange_poly([-1,0,1,2],[1,2,4,20])
+k = [1,5]
+l = [1,6]
+m = [1,2,3]
+n = [1,2,8]
+o = [1,2,6,20,25]
+p = [1,2,3,4,5]
 
-@benchmark polyfit(   [-1,0,1],[1,2,4])
-@benchmark createpoly([-1,0,1],[1,2,4])
+#first order poly
+@benchmark polyfit(k,l) # 1e-6 s
+@benchmark create_lagrange_poly(k,l) # 3e-7 s
+@benchmark createpolyOld(k,l) # 1.4 e-7 s
+@benchmark createpoly(k,l) # 1.4 e-7 s
+
+#second order poly
+@benchmark polyfit(m,n) # 1.6 e-6 s
+@benchmark create_lagrange_poly(m,n) 
+@benchmark createpolyOld(m,n)
+@benchmark createpoly(m,n)
+
+
+#third order poly
+@polyfit(o,p)
+@benchmark create_lagrange_poly(o,p)
+@benchmark createpolyOld(o,p)
+@createpoly(o,p)
